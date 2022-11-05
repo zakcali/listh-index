601e77c2f568e32d476927cfd3ef888f536db1cc
Zafer Akçalı <zaferakcali@yahoo.com>
2022-11-05 17:44:01 +0300

    Creates h-index list and saves

---
+++ main.js
@@ -0,0 +1,80 @@
+//Electron setup
+const { app, BrowserWindow ,ipcMain} = require('electron')
+const path = require('path')
+const electron = require('electron')
+const fetch = require('node-fetch-commonjs').default
+const fs = require('fs')
+const { dialog } = require('electron')
+
+async function createWindow () {
+  // Create the browser window.
+global.win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    webPreferences: {
+		nodeIntegration: true,
+		nativeWindowOpen: true,
+		enableRemoteModule: true,
+		worldSafeExecuteJavaScript: true,
+		preload: path.join(__dirname, 'preload.js'),
+    }
+  })
+// global.win.webContents.on("devtools-opened", () => { win.webContents.closeDevTools(); });
+global.win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+// https://stackoverflow.com/questions/61161341/electron-dialog-not-saving-file
+ipcMain.handle('save', async (event, saveText) => {
+  const { filePath, canceled } = await dialog.showSaveDialog({
+    defaultPath: "output.csv"
+  });
+
+  if (filePath && !canceled) {
+    const data = new Uint8Array(Buffer.from(saveText));
+    fs.writeFile(filePath, data, (err) => {
+      if (err) throw err;
+      return ('The file has been saved!');
+    });
+  }
+	})
+
+ipcMain.handle('checkHindex', async (event, rid) => {
+let returnText='\"'+'https://www.webofscience.com/wos/author/rid/'+rid+'\"'
+url = 'https://www.webofscience.com/wos/api/proxy/wos-researcher/stats/individual/' + rid +'/' 
+const response = await fetch(url);
+try {
+const data = await response.json();
+// console.log(data);
+if (data['detail']=="Not found.")
+	return (returnText+'\t'+rid+'\tn/a')
+returnText=returnText+'\t'+rid+'\t'+data['hIndex']+'\t'+data['timesCited']+'\t'+data['numPublicationsInWosCc']
+for (i=2022; i>1960; i--) {
+	let cpyear = data['citationsPerYear'][i.toString()]
+		if (cpyear)
+			returnText=returnText+'\t'+cpyear
+	}
+return (returnText)
+}
+catch(err) {
+	return (returnText+'\t'+rid+'\terror')
+}
+
+})
+
+
+
+
---
+++ renderer.js
@@ -0,0 +1,33 @@
+const { ipcRenderer } = require('electron')
+const researcherIdsArea = document.querySelector ('#researcherIds');
+const findBtn = document.querySelector ('#find');
+const saveBtn = document.querySelector ('#save');
+const infoText = document.querySelector ('#infoTextOutput');
+
+findBtn.addEventListener('click', async () => {
+infoText.innerHTML = '' // delete any previous information about row counts
+let	areaText='URL\tResearcherID\tH-index\tCitations\tPapers\t2022\t2021'
++'\t2020\t2019\t2018\t2017\t2016\t2015\t2014\t2013\t2012\t2011\t2010\t2009\t2008\t2007\t2006\t2005\t2004\t2003\t2002\t2001'
++'\t2000\t1999\t1998\t1997\t1996\t1995\t1994\t1993\t1992\t1991\t1990\t1989\t1988\t1987\t1986\t1985\t1984\t1983\t1982\t1981'
++'\t1980\t1979\t1978\t1977\t1976\t1975\t1974\t1973\t1972\t1971\t1970\t1969\t1968\t1967\t1966\t1965\t1964\t1963\t1962\t1961'
+
+const ridText = researcherIdsArea.value
+const ridArray = ridText.replace(/[^\x00-\x7F]/g,'').replace(/\r\s/g, '').split('\n'); // text to array
+let i=0;
+for (let ele of ridArray ) {
+	if (ele !=='') {
+	ipcresult = await ipcRenderer.invoke('checkHindex', ele.replace(/\s/g, '').trim().toUpperCase()); 
+	areaText=areaText+'\n'+ipcresult;
+	researcherIdsArea.value=areaText;
+	i++;
+	infoText.innerHTML = i;
+	}
+}
+infoText.innerHTML = 'Atıfı hesaplanan yazar sayısı = '+i;
+
+});
+
+saveBtn.addEventListener('click', async () => {
+const	ipcresult = await ipcRenderer.invoke('save', researcherIdsArea.value); 
+	});
+
